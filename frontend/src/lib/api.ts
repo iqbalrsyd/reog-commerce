6a5907e934f321271f11441dc44a918ec47821e6
iqbalrsyd <iqbalhidayatrasyad@mail.ugm.ac.id>
2025-11-15 00:16:21 +0700

    change url backend for deployment

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust this to your backend URL
+  baseURL: process.env.VITE_API_URL, // Adjust this to your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
